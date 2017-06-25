c4841308dca0adce83097c5e4018ee47f2317788
Dominik Fischer <uqebd@student.kit.edu>
2017-06-25 21:06:52 +0200

    Cleaned up a bit

--- rgb_control.js
+++ rgb_control.js
@@ -1,57 +1,49 @@
 'use strict';
+
 var exec = require('child_process').exec;
 
-const _red_port = 17;                                                           //GPIO PORT 17 RED
-const _green_port = 24;                                                         //GPIO PORT 24 GREEN
-const _blue_port = 22;                                                          //GPIO PORT 22 BLUE
+//GPIO PORT 17 RED
+const _red_pin = 17;
+//GPIO PORT 24 GREEN
+const _green_pin = 24;
+//GPIO PORT 22 BLUE
+const _blue_pin = 22;
 
 var set = function(rgb) {
-  //pigs p *PORT* *VALUE* to set Intensity where 0 <= *VALUE* <= 255
-  if(rgb.red & rgb.green & rgb.blue) {
-    var command = 'pigs p '+ _red_port + " " + rgb.red
-              + ' & pigs p ' + _green_port + " " + rgb.green
-              + ' & pigs p ' + _blue_port + " " + rgb.blue;
-
-    exec('command', (error, stdout, stderr) => {
-      if(!error) {
-        console.log(stdout);
-      } else {
-        console.log('There was an Error using pigpio: stderr:' + stderr);
-        console.log(error.message);
-      }
-    });
+  if (rgb.red & rgb.green & rgb.blue) {
+    setPinLevel(_red_pin, rgb.red);
+    setPinLevel(_green_pin, rgb.green);
+    setPinLevel(_blue_pin, rgb.blue);
   }
 }
 var get = function() {
-  //pigs gdc *PORT* to get Intensity where 0 <= *VALUE* <= 255
-  var level = {red: 0, green : 0, blue : 0};
-
-  var commands = {red : 'pigs gdc '+ _red_port,
-              green : 'pigs gdc '+ _green_port,
-              blue : 'pigs gdc '+ _blue_port};
-
-  exec(commands.red, (error, stdout, stderr) => {
-    if(!error) {
-      level.red = stdout;
-    } else {
-      console.log(error.message);
-    }
-  });
-  exec(commands.green, (error, stdout, stderr) => {
-    if(!error) {
-      level.green = stdout;
-    } else {
-      console.log(error.message);
-    }
-  });
-  exec(commands.blue, (error, stdout, stderr) => {
-    if(!error) {
-      level.blue = stdout;
-    } else {
-      console.log(error.message);
-    }
-  });
-  return level;
+  return {red : getPinLevel(_red_pin) ,
+          green : getPinLevel(_green_pin) ,
+          blue : getPinLevel(_blue_pin)};
 };
+
+var getPinLevel = function(pin) {
+//pigs gdc *PORT* to get Intensity where 0 <= *VALUE* <= 255
+  exec('pigs gdc ' + pin, (error, stdout, stderr) => {
+    if (!error) {
+      return stdout;
+    } else {
+      console.log('There was an Error using pigpio: stderr:' + stderr);
+      console.log(error.message);
+    }
+  });
+}
+
+var setPinLevel = function(pin , level) {
+//pigs p *PORT* *VALUE* to set Intensity where 0 <= *VALUE* <= 255
+  exec('pigs p ' + pin + " " + level, (error, stdout, stderr) => {
+    if (!error) {
+      console.log(stdout);
+    } else {
+      console.log('There was an Error using pigpio: stderr:' + stderr);
+      console.log(error.message);
+    }
+  });
+}
 
 module.exports = {set : set, get : get};
