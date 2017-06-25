22ea9fde48632e3933b40b3d1b5da8b0e84d4baf
Dominik Fischer <uqebd@student.kit.edu>
2017-06-25 22:39:51 +0200

    Bit more nody

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ const app = express();
 app.use(logging);
 
 app.get('/:red/:green/:blue', (req, res) => {
-  var rgb = {red : req.params.red - 0, green : req.params.green - 0, blue :req.params.blue- 0}
+  var rgb = {red : req.params.red - 0, green : req.params.green - 0, blue :req.params.blue - 0}
   if ( 0 <= rgb.red <= 255 && 0 <= rgb.green <= 255 && 0 <= rgb.blue <= 255) {
     rgb_control.set(rgb);
     res.send(rgb);
   }
 });
 app.get('/', (req, res) => {
-  res.send(rgb_control.get());
+  (() => res.send(rgb_control.get()));
 });
 
 const server = http.createServer(app);
